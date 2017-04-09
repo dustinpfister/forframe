c671565631b8cd00cdff2ae8b28f5002f59ee9d6
dustinpfister <dustin.pfister@gmail.com>
2017-04-08 20:10:34 -0400

    new scene

--- views/js/scene.js
+++ views/js/scene.js
@@ -1,100 +1,14 @@
 /*
 
-    scene.js demo for forframe.js
+scene.js demo for forframe.js
 
-*/
-var distance = function (x1, y1, x2, y2) {
+ */
 
-    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
-
-},
-
-blob2d = function (method) {
-
-    var parts = [],
-
-    state = {
-        size : 16,
-        pxSize : 8,
-        space : 1,
-        offset : {
-
-            x : 10,
-            y : 30
-
-        }
-    },
-    x = 0,
-    y = 0,
-    cx = state.size / 2,
-    cy = state.size / 2,
-    px = 0;
-
-    // build the parts
-    while (px < state.size * state.size) {
-
-        x = px % state.size;
-        y = Math.floor(px / state.size);
-
-        if (distance(x, y, cx, cy) < (state.size - 1) / 2) {
-
-            parts.push({
-
-                id : 'blobpx_' + px,
-                w : state.pxSize,
-                h : state.pxSize,
-                //x : x * (state.pxSize + state.space) + state.offset.x,
-                //y : y * (state.pxSize + state.space) + state.offset.y,
-
-                forFrame : function (pt) {
-
-                    state.cx = cx;
-                    state.cy = cy;
-                    state.px = pt.id.split('_')[1];
-                    state.bias = 1 - Math.abs(.5 - this.percentDone) / .5;
-                    state.x = state.px % state.size;
-                    state.y = Math.floor(state.px / state.size);
-                    state.d = distance(state.x, state.y, state.cx, state.cy);
-
-                    method.call(this, pt, state);
-
-                    pt.w = state.pxSize;
-                    pt.h = state.pxSize;
-                    pt.x = (state.x - 1) * (state.pxSize + state.space) + state.offset.x;
-                    pt.y = (state.y - 1) * (state.pxSize + state.space) + state.offset.y;
-
-                },
-                skin : {
-
-                    appendRender : function (ctx, skin) {
-
-                        var pt = skin.part;
-
-                        ctx.fillStyle = '#00ffff';
-                        ctx.fillRect(0, 0, pt.w, pt.h);
-
-                    }
-
-                }
-
-            });
-
-        }
-
-        px += 1;
-    }
-
-    // return the parts
-    return parts;
-
-};
-
-//
 scene({
 
-    projectName : 'blob_2',
+    projectName : 'demo1',
 
-    maxFrame : 10,
+    maxFrame : 100,
 
     viewPort : {
 
@@ -116,20 +30,95 @@ scene({
         }
     },
 
-    parts : blob2d(function (pt, s) {
+    parts : [{
 
-        s.pxSize = 12;
-        s.space = 10;
-        cx = this.viewPort.w / 2 - (s.size - 1) / 2 * (s.pxSize+s.space),
-        cy = this.viewPort.h / 2 - (s.size - 1) / 2 * (s.pxSize+s.space),
-        radian = s.px / (s.size * 2) * Math.PI/2 * s.bias;
+            id : 'd',
+            w : 32,
+            h : 32,
+            forFrame : function (pt) {
 
-        s.offset.x = cx + Math.cos(radian) * 3;
-        s.offset.y = cy + Math.sin(radian) * 3;
+                pt.w = 800 - 700 * this.percentDone;
+                pt.h = 700 - 600 * this.percentDone;
 
-        pt.opacity = 1 - s.d / (s.size / 2);
+                pt.x = 240 - pt.w / 2 - pt.w;
+                pt.y = 180 - pt.h / 2;
 
-    }),
+            },
+            skin : {
+                imgIndex : 1,
+                sx : 0,
+                appendRender : function (ctx, skin) {
+
+                    var pt = skin.part;
+
+                    //ctx.fillStyle = '#ff0000';
+                    //ctx.fillRect(0, 0, pt.w, pt.h);
+
+                }
+
+            }
+
+        }, {
+
+            id : 'j',
+            w : 32,
+            h : 32,
+            forFrame : function (pt) {
+
+                pt.w = 800 - 700 * this.percentDone;
+                pt.h = 700 - 600 * this.percentDone;
+
+                pt.x = 240 - pt.w / 2;
+                pt.y = 180 - pt.h / 2;
+
+            },
+            skin : {
+                imgIndex : 1,
+                sx : 32,
+                appendRender : function (ctx, skin) {
+
+                    var pt = skin.part;
+
+                    //skin.sx = 32 + 32 * this.percentDone;
+
+                    //ctx.fillStyle = '#00ff00';
+                    //ctx.fillRect(0, 0, pt.w, pt.h);
+
+                }
+
+            }
+
+        }, {
+
+            id : 'p',
+            w : 32,
+            h : 32,
+            forFrame : function (pt) {
+
+                pt.w = 800 - 700 * this.percentDone;
+                pt.h = 700 - 600 * this.percentDone;
+
+                pt.x = 240 - pt.w / 2 + pt.w;
+                pt.y = 180 - pt.h / 2;
+
+            },
+            skin : {
+
+                imgIndex : 1,
+                sx : 64,
+                appendRender : function (ctx, skin) {
+
+                    var pt = skin.part;
+
+                    //ctx.fillStyle = '#0000ff';
+                    //ctx.fillRect(0, 0, pt.w, pt.h);
+
+                }
+
+            }
+
+        }
+    ],
 
     // define the forFrame movement
     forFrame : function () {}
@@ -141,7 +130,8 @@ scene.injectCanvas('ui_display');
 
 scene.load(
     [
-        'img/mylogo_128.png'
+        'img/mylogo_128.png',
+        'img/sheet1.png'
     ],
     function (progress) {
 
